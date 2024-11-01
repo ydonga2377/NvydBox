875c277af63b28aaadb0689921ccac07fb19a16c
Vasu <vasubhalodia@gmail.com>
2024-11-01 07:48:59 -0400

    cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Common/header';
-import Footer from './components/Common/Footer';
-import HomePage from './components/Home';
-import GamesPage from './components/Game';
-import MarketplacePage from './components/Marketplace';
-import CommunityPage from './components/Community';
-import LibraryPage from './components/Library';
-import SignInPage from './components/Singin';
-import SingleGame from './components/SingleGame';
-import UserProfilePage from './components/User-profile';
-import AboutUsPage from './components/AboutUs'
-import Register from './components/Register/index';
-import ForgotPassword from './components/Forgot-password';
-import ResetPassword from './components/Reset-password';
-import "./assets/css/bootstrap.min.css"
-import "./App.css"
-import "./assets/css/style.css"
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Common/header";
+import Footer from "./components/Common/footer";
+import HomePage from "./components/Home";
+import GamesPage from "./components/Game";
+import MarketplacePage from "./components/Marketplace";
+import CommunityPage from "./components/Community";
+import LibraryPage from "./components/Library";
+import SignInPage from "./components/Singin";
+import SingleGame from "./components/SingleGame";
+import UserProfilePage from "./components/User-profile";
+import AboutUsPage from "./components/AboutUs";
+import Register from "./components/Register/index";
+import ForgotPassword from "./components/Forgot-password";
+import ResetPassword from "./components/Reset-password";
+import CartPage from "./components/Cart";
+import "./assets/css/bootstrap.min.css";
+import "./App.css";
+import "./assets/css/style.css";
 
 function App() {
   return (
@@ -37,9 +36,9 @@ function App() {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/userprofile" element={<UserProfilePage />} />
         <Route path="/AboutUs" element={<AboutUsPage />} />
-
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
